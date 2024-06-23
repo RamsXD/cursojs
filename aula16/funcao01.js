df7ed019c90ad07e891dab56d9ad6f5b8409fc1c
Alessandro Ramiro <ramsxd67@gmail.com>
2024-06-23 18:29:29 -0300

    funcao

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpa (n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+//console.log(parimpa(4))
+let res = parimpa(22)
+console.log(res)
